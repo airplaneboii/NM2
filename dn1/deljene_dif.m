4c6cb1e6c6cedd71702667b5c57febfb91a4b7b3
airplaneboii <88340299+airplaneboii@users.noreply.github.com>
2023-04-14 10:40:26 +0200

    hw completed

--- dn1/deljene_dif.m
+++ dn1/deljene_dif.m
@@ -8,5 +8,20 @@ function d = deljene_dif(X,Y,dY)
 %
 % Izhod:
 %   d    izracunane deljene diference (koeficienti polinoma)
+    
+    n = length(X)-1;
+    M = zeros(n+1);
+    M(:,1) = Y';
+    
+    for j=2:n+1
+        for i=j:n+1
+            if (X(i-j+1) == X(i))
+                M(i, j) = dY(i);
+            else
+                M(i,j) = (M(i,j-1)-M(i-1,j-1))/(X(i)-X(i-j+1));
+            end
+        end
+    end
+    d=diag(M);
 
 end
--- dn1/horner.m
+++ dn1/horner.m
@@ -9,7 +9,11 @@ function v = horner(X,d,t)
 % Izhod:
 %   v    vrednosti polinoma p v tockah iz seznama t
 %
-
+    n = length(X)-1;
+    v = d(n+1);
+    for i = n:-1:1
+        v = d(i)+(t-X(i)).*v;
+    end
 end
 
     
